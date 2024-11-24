774f7314eb8b2f42f254efb35e7f11e03a3f8495
yousuf-git <hackerbest052@gmail.com>
2024-11-24 19:31:48 +0500

    "Added config for CORS issue"

---
+++ src/main/java/com/project/hospital/configs/WebConfig.java
@@ -0,0 +1,22 @@
+package com.project.hospital.configs;
+
+// import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Allow CORS for all endpoints, from all origins, with specific methods allowed
+        registry.addMapping("/**")
+        // Allow all origins
+                .allowedOrigins("http://localhost:4200") // You can replace with your frontend URL
+                // .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
